eecb7a0619a5c4f5aeeffd6cb8ec0e7eaf068fee
Julian <julian_sempre@hotmail.com>
2023-05-12 16:14:26 -0300

    initial commit

---
+++ src/main/java/com/mycompany/calculadora/java/CalculadoraJava.java
@@ -0,0 +1,12 @@
+package com.mycompany.calculadora.java;
+
+import com.mycompany.calculadora.java.view.CalculadoraView;
+
+public class CalculadoraJava {
+
+    public static void main(String[] args) {
+        System.out.println("Iniciando!");
+        CalculadoraView C = new CalculadoraView();
+        C.setVisible(true);
+    }
+}
